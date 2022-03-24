337ca54266e86842a50fc53c1b05ce74dec453b7
leticia-f <leticia.fonseca.1106@gmail.com>
2022-03-24 15:24:03 -0300

    bondinho e idade - 24.03

---
+++ bondinho.py
@@ -0,0 +1,9 @@
+# nível júnior - 2017 - fase 1
+
+A = int(input()) # converter str (input padrão) em int
+M = int(input())
+
+if (A + M)>50:
+    print("N")
+else:
+    print("S")
---
+++ idade.py
@@ -0,0 +1,13 @@
+# nível júnior - 2019 - fase 1
+
+M = int(input())
+A = int(input())
+B = int(input())
+C = M-(A+B)
+
+if A > B and A > C:
+    print(A)
+elif B > A and B > C:
+    print(B)
+else:
+    print(C)
